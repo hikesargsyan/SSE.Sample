996cb1fa8ac68fdb0f1ba82c05d5c8f512895eca
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-09-27 17:28:07 +0400

    added: EncodeTextCommand

---
+++ src/Application/Processing/Commands/EncodeText/EncodeTextCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace OneInc.Server.Application.Processing.Commands.EncodeText;
+
+public class EncodeTextCommand : IRequest<string>
+{
+    public EncodeTextCommand(string text)
+    {
+        Text = text;
+    }
+
+    public string Text { get; }
+}
---
+++ src/Application/Processing/Commands/EncodeText/EncodeTextCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace OneInc.Server.Application.Processing.Commands.EncodeText;
+
+public class EncodeTextCommandHandler : IRequestHandler<EncodeTextCommand, string>
+{
+    public Task<string> Handle(EncodeTextCommand request, CancellationToken cancellationToken)
+    {
+        var charsCountMap = new Dictionary<char, int>();
+        foreach (var c in request.Text)
+        {
+            if (!charsCountMap.TryAdd(c, 1))
+            {
+                charsCountMap[c]++;
+            }
+        }
+
+        var resultBuilder = new StringBuilder();
+        foreach (var kvp in charsCountMap.OrderBy(kvp => kvp.Key))
+        {
+            resultBuilder
+                .Append(kvp.Key)
+                .Append(kvp.Value);
+        }
+
+        var base64Encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(request.Text));
+
+        resultBuilder
+            .Append('/')
+            .Append(base64Encoded);
+
+        return Task.FromResult(resultBuilder.ToString());
+    }
+}
