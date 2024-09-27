7bb06468c63555cc324cb4ca152cb8e4fe48d52d
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-09-27 19:21:01 +0400

    added: EncodeTextCommandValidator

---
+++ src/Application/Processing/Commands/EncodeText/EncodeTextCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using OneInc.Server.Domain.Constants;
+
+namespace OneInc.Server.Application.Processing.Commands.EncodeText;
+
+public class EncodeTextCommandValidator : AbstractValidator<EncodeTextCommand>
+{
+    public EncodeTextCommandValidator()
+    {
+        RuleFor(x => x.Text)
+            .Must(s => !string.IsNullOrWhiteSpace(s) )
+            .WithMessage(ValidationErrorCode.NotNullOrEmpty);
+    }
+}
