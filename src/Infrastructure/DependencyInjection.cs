c27d81da8feb893c26a2e834c1b82b9aec86a942
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-09-27 19:21:43 +0400

    added: RedisCacheService

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OneInc.Server.Application.Common.Interfaces;
+using OneInc.Server.Application.Common.Services;
 
 namespace OneInc.Server.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment )
     {
+        
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration["Redis:Endpoint"];
+            options.InstanceName = configuration["Redis:Instance"];
+        });
+
+        services.AddSingleton<ICacheService, RedisCacheService>();
         
         return services;
     }
