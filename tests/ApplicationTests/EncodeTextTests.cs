421cd4f59cf0f9c448df5095cafd86a0f337e1d8
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-10-02 16:59:44 +0400

    added EncodeTextTests

---
+++ tests/ApplicationTests/EncodeTextTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using OneInc.Server.Application.Common.Constants;
+using OneInc.Server.Application.TextProcessing.Commands.EncodeText;
+using OneInc.Server.Application.TextProcessing.Commands.ProcessText;
+
+namespace ApplicationTests;
+
+public class EncodeTextTests
+{
+
+    [Theory]
+    [InlineData("Hello, World!", " 1!1,1H1W1d1e1l3o2r1/SGVsbG8sIFdvcmxkIQ==")]
+    [InlineData("a", "a1/YQ==")]
+    public async Task Should_Return_Proper_Encoded_Data(string input, string output)
+    {
+        var commandHandler = new EncodeTextCommandHandler();
+
+        var result = await commandHandler.Handle(new EncodeTextCommand(input), new CancellationToken());
+
+        result.Should().Be(output);
+    }
+    
+}
